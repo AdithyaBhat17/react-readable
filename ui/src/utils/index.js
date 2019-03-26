3169c5bf85f07ba8895b4592d40141d6b50580ad
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-03-26 22:13:05 +0530

    :zap: Added api calls to fetch posts, vote for a post, add a post

--- ui/src/utils/index.js
+++ ui/src/utils/index.js
@@ -11,6 +11,16 @@ const getHeaders = {
     }
 }
 
+const postHeaders = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": header
+    }
+}
+
+
+
 export const getAllPostsAPI = (category = undefined) => {
     if(category !== undefined) {
         return fetch(`${devURL}/${category}/posts`, getHeaders)
@@ -19,3 +29,25 @@ export const getAllPostsAPI = (category = undefined) => {
     return fetch(`${devURL}/posts`, getHeaders)
     .then(response => response.json())
 } 
+
+export const getCategoriesAPI = () => {
+    return fetch(`${devURL}/categories`, getHeaders)
+    .then(response => response.json())
+}
+
+export const getPostAPI = (id) => {    
+    return fetch(`${devURL}/posts/${id}`, getHeaders)
+    .then(response => response.json())
+}
+
+export const createPostAPI = (body) => {
+    postHeaders.body = JSON.stringify(body)
+    return fetch(`${devURL}/posts`, postHeaders)
+    .then(response => response.json())
+}
+
+export const votePostAPI = (id, option) => {
+    postHeaders.body = JSON.stringify({option})
+    return fetch(`${devURL}/posts/${id}`, postHeaders)
+    .then(response => response.json())
+}
