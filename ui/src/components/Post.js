93e2ba3f73a56eda180a7306db2d057ab1b90ab7
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-04-12 21:13:13 +0530

    :sparkles: refactored comments and post page

--- ui/src/components/Post.js
+++ ui/src/components/Post.js
@@ -8,6 +8,7 @@ import { withRouter } from 'react-router-dom'
 import { Form } from 'react-bootstrap'
 import { generateId } from '../utils'
 import { delete_post } from './Posts'
+import Comments from './Comments'
 
 const Post = (props) => {
     const [commentBox, setCommentBox] = React.useState(false)
@@ -45,6 +46,8 @@ const Post = (props) => {
             body: e.target.body.value,
             author: e.target.author.value,
         })
+        e.target.body.value = ''
+        e.target.author.value = ''
     }
 
     const vote_comment = (id, option) => {
@@ -114,45 +117,16 @@ const Post = (props) => {
             <br/>
             {/* TODO: Refactor  */}
             {comments && comments.sort((a,b) => b.timestamp - a.timestamp).map(comment => (
-                <div style={{
-                    display: 'flex', 
-                    justifyContent: 'space-between',
-                    boxShadow: '0px 1px 3px 0 rgba(0,0,0,0.07)',
-                    padding: 10,
-                    backgroundColor: '#f7f8f3',
-                    marginBottom: 15,
-                    borderRadius: 6
-                }} key={comment.id}>
-                    <div>
-                        {!commentBox ? <h6>{comment.body}</h6> : 
-                        <Form style={{display: 'inline-flex'}} onSubmit={(e) => edit_comment(comment.id, e)}>
-                            <Form.Control name="edit" type="text" defaultValue={comment.body} /> &nbsp;
-                            <Button style={{backgroundColor: '#f7444e', border: 'none'}} type="submit">
-                                Comment
-                            </Button>
-                        </Form>}
-                        <p><small>by <strong>{comment.author}</strong> on {getDate(comment.timestamp)}</small></p>
-                        <ButtonGroup>
-                            <Button onClick={() => vote_comment(comment.id,'upVote')} style={{backgroundColor: '#78bcc4', border: 'none'}}>
-                                <i className="fa fa-thumbs-up"></i>
-                            </Button>
-                            <Button onClick={() => vote_comment(comment.id,'downVote')} style={{backgroundColor: '#f7444e', border: 'none'}}>
-                                <i className="fa fa-thumbs-down"></i>
-                            </Button>
-                        </ButtonGroup> 
-                        &nbsp;&nbsp;{comment.voteScore}
-                    </div>
-                    <div>
-                        <ButtonGroup>
-                            <Button onClick={() => setCommentBox(true)} style={{backgroundColor: '#78bcc4', border: 'none'}}>
-                                <i className="fa fa-edit"></i>
-                            </Button>
-                            <Button onClick={() => delete_comment(comment.id)} style={{backgroundColor: '#f7444e', border: 'none'}}>
-                                <i className="fa fa-trash-alt"></i>
-                            </Button>
-                        </ButtonGroup> 
-                    </div>
-                </div>
+                <Comments 
+                 commentBox={commentBox}
+                 setCommentBox={setCommentBox}
+                 comment={comment}
+                 key={comment.id}
+                 vote_comment={vote_comment}
+                 delete_comment={delete_comment}
+                 edit_comment={edit_comment}
+                 getDate={getDate}
+                />
             ))}
         </div>
     )
